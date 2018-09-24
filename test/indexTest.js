024e937853ba158180ec592d22a03ff09657a039
winton <mail@wintoni.us>
2018-09-24 12:13:20 -0400

    Upgrade packages

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,8 +1,8 @@
-import Store from "dot-store"
+import dotStore from "dot-store"
 import spawn from "../dist/spawn"
 
 test("spawn command", async () => {
-  const store = spawn(new Store())
-  await store.set("spawn.test.command", ["echo", "hi"])
-  expect(store.get("spawn.test.output")).toBe("hi\r\n")
+  const store = spawn(dotStore())
+  await store.spawn("test", "echo", ["hi"])
+  expect(store.get("test")).toBe("hi\r\n")
 })
