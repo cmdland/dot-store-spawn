ece69bed21ee0044a354bc8fbda946ad5fba587e
winton <mail@wintoni.us>
2018-11-25 16:26:37 -0500

    Upgrade @dot-event/store

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,19 +1,20 @@
 import dotEvent from "dot-event"
 import dotStore from "@dot-event/store"
-import spawn from "../dist/spawn"
+
+import dotSpawn from "../"
 
 test("spawn command", async () => {
   const events = dotEvent()
-  const store = dotStore({ events })
 
-  spawn({ events, store })
+  dotSpawn({ events })
+  dotStore({ events })
 
   await events.spawn("test", {
     args: ["hi"],
     command: "echo",
   })
 
-  expect(store.get("test")).toMatchObject({
+  expect(events.get("test")).toMatchObject({
     args: ["hi"],
     code: 0,
     command: "echo",
@@ -25,16 +26,16 @@ test("spawn command", async () => {
 
 test("spawn command with options", async () => {
   const events = dotEvent()
-  const store = dotStore({ events })
 
-  spawn({ events, store })
+  dotSpawn({ events })
+  dotStore({ events })
 
   await events.spawn("test", {
     command: "pwd",
     cwd: "/",
   })
 
-  expect(store.get("test")).toMatchObject({
+  expect(events.get("test")).toMatchObject({
     code: 0,
     command: "pwd",
     cwd: "/",
